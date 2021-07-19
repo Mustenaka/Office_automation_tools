99ab79f6656127c0cb43050534ef563d05162ba3
Andrew·Redfiled·Wong <31812641+Mustenaka@users.noreply.github.com>
2021-07-20 00:49:21 +0800

    自动命名脚本 - 完成正则表达式

--- 批量照片命名/自动命名脚本.bat
+++ 批量照片命名/自动命名脚本.bat
@@ -1,6 +1,6 @@
 set /a n=0
 setlocal enabledelayedexpansion
-for /F %%i in ('dir /a/b ^| findstr .[jpg]$') do (
+for /F %%i in ('dir /a/b ^| findstr /e /R  /c:".[jpg|png|gif|jpeg]"') do (
     set /a n+=1
     echo %%i>>temp.txt
 )
